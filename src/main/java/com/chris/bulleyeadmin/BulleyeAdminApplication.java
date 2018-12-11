9b293f14341e572bcab935c33256fe749ccac611
961860916@qq.com <8302812>
2018-12-11 15:26:59 +0800

    修改服务不能启动bug

--- src/main/java/com/chris/bulleyeadmin/BulleyeAdminApplication.java
+++ src/main/java/com/chris/bulleyeadmin/BulleyeAdminApplication.java
@@ -2,8 +2,10 @@ package com.chris.bulleyeadmin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import tk.mybatis.spring.annotation.MapperScan;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class BulleyeAdminApplication {
 
     public static void main(String[] args) {
