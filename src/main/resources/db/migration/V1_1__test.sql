4543f2d339599702ce7ef1f6f2f7071b1558a6ae
961860916@qq.com <8302812>
2018-12-12 17:38:51 +0800

    增加flyway依赖

---
+++ src/main/resources/db/migration/V1_1__test.sql
@@ -0,0 +1,23 @@
+-- 创建表demo，使用存储过程进行判断，用完后删除
+drop procedure if exists createHolidays;
+delimiter $$
+create procedure createHolidays() begin
+  if not exists (select * FROM information_schema.columns WHERE table_schema = DATABASE()  AND table_name = 'kb_holidays') then
+    create table kb_holidays
+    (
+       id                   varchar(40) not null,
+       hospital_id          varchar(40),
+       start_date           varchar(20),
+       end_date             varchar(20),
+       type_id              varchar(40) comment 'HolidayType',
+       created              timestamp default CURRENT_TIMESTAMP,
+       user_id              varchar(40),
+       status               int default 0,
+       primary key (id)
+    );
+
+  end if;
+end $$
+delimiter ;
+call createHolidays();
+drop procedure if exists createHolidays;
