ff5fdaac6bd7875555258d3260303c2d40567e31
Chris <961860916@qq.com>
2018-12-12 23:41:53 +0800

    集成接口插件swagger

--- src/main/java/com/chris/bulleyeadmin/controller/LoginController.java
+++ src/main/java/com/chris/bulleyeadmin/controller/LoginController.java
@@ -2,10 +2,12 @@ package com.chris.bulleyeadmin.controller;
 
 import com.chris.bulleyeadmin.pojo.Account;
 import com.chris.bulleyeadmin.pojo.User;
-import com.chris.bulleyeadmin.security.MyInvocationSecurityMetadataSourceService;
 import com.chris.bulleyeadmin.service.AccountService;
 import com.chris.bulleyeadmin.utils.AuthUtil;
 import com.chris.bulleyeadmin.utils.Operalog;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.stereotype.Controller;
@@ -18,6 +20,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
  */
 @Operalog("登陆")
 @Controller
+@Api("swaggerDemoController相关的api")
 public class LoginController {
 
 
@@ -36,12 +39,10 @@ public class LoginController {
     }
 
     @Operalog("进入首页")
+    @ApiOperation(value = "根据id查询学生信息", notes = "查询数据库中某个的学生信息")
+    @ApiImplicitParam(name = "id", value = "学生ID", paramType = "path", required = true, dataType = "Integer")
     @RequestMapping(value = "/home", method = RequestMethod.GET)
     public String index(Model view) {
-        //授权校验
-        /*if(!licenseCheck()) {
-            return "setup";
-        }*/
         Account account = new Account();
         account.setEmail("961860916@qq.com");
         account.setPassword(PasswordEncoderFactories.createDelegatingPasswordEncoder().encode("1"));
