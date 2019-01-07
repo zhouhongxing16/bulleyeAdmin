90f2a71edc2ecc85c5a495ffd53bb6363df6da0f
lihongshu <lhslxl@qq.com>
2019-01-07 21:36:50 +0800

    微信表

---
+++ src/main/java/com/chris/bulleyeadmin/wechat/WeixinServlet.java
@@ -0,0 +1,24 @@
+package com.chris.bulleyeadmin.wechat;
+
+import com.chris.bulleyeadmin.common.utils.Operalog;
+import com.github.sd4324530.fastweixin.servlet.WeixinServletSupport;
+import com.github.sd4324530.fastweixin.servlet.WeixinSupport;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * 
+ * @author lhs 2019-01-07
+ *
+ */
+@Controller
+@RequestMapping("/weChat")
+@Operalog("微信官方消息拦截器")
+public class WeixinServlet extends WeixinServletSupport{
+
+	@Override
+	protected WeixinSupport getWeixinSupport() {
+		return new MyWeixinSupportServlet();
+	}
+
+}
