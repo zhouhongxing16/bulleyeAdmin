86a6d52c848b264c883afbdab39865b292577250
Chris <961860916@qq.com>
2019-01-08 20:12:04 +0800

    重构后台前端模版

--- src/main/resources/static/assets/chris/js/vue-component.js
+++ src/main/resources/static/assets/chris/js/vue-component.js
@@ -1,5 +1,47 @@
+/*
+
+Vue.component("left-menu",{
+    template:`
+    <div id="side-nav">
+        <ul id="nav">
+            <li v-for="(item,i) in data">
+                    <a href="javascript:;" v-bind:_href="item.path" v-bind:id="item.id">
+                        <i class="iconfont">&#xe6b8;</i>
+                        <cite>{{item.name}}</cite>
+                        <i v-if="item.children!=null" class="iconfont nav_right">&#xe697;</i>
+                    </a>
+                    <children-menu v-if="item.children!=null" v-bind:data="item.children"></children-menu>
+             </li>
+        </ul>
+    </div>`,
+    props:{
+        data: {
+            type: Array,
+            default: []
+        }
+    }
+});
+Vue.component("children-menu",{
+    template:`
+            <ul class="sub-menu">
+                <li v-for="(item,i) in data">
+                    <a v-bind:_href="item.path" v-bind:id="item.id">
+                        <i class="iconfont">&#xe6a7;</i>
+                        <cite>{{item.name}}</cite>
+                    </a>
+                    <children-menu v-if="item.children!=null" v-bind:data="item.children"></children-menu>
+                </li >
+            </ul>`,
+    props:{
+        data: {
+            type: Array,
+            default: []
+        }
+    }
+});
+*/
 Vue.component("left-menu", {
-    template: "\n    <div id=\"side-nav\">\n        <ul id=\"nav\">\n            <li v-for=\"(item,i) in data\">\n                    <a href=\"javascript:;\" v-bind:_href=\"item.path\" v-bind:id=\"item.id\">\n                        <i class=\"iconfont\">&#xe6b8;</i>\n                        <cite>{{item.name}}</cite>\n                        <i v-if=\"item.children!=null\" class=\"iconfont nav_right\">&#xe697;</i>\n                    </a>\n                    <children-menu v-if=\"item.children!=null\" v-bind:data=\"item.children\"></children-menu>\n             </li>\n        </ul>\n    </div>",
+    template: "\n        <div class=\"layui-side-scroll\">\n            <ul class=\"layui-nav layui-nav-tree\" id=\"ul\" style=\"background-color:#20222a\">\n                    <!--\u8FD9\u91CC\u53EF\u4EE5\u5199\u83DC\u5355\u7ED3\u6784,\u4E5F\u53EF\u4EE5\u653E\u7A7A,\u4F7F\u7528\u5F02\u6B65\u52A0\u8F7D-->\n                    <span class=\"layui-nav-bar\" style=\"height: 0px; top: 0px; opacity: 0;\"></span>\n                    <li class=\"lau-nav-item\" v-for=\"(item,i) in data\">\n                        <a class=\"lau-nav-header\" v-bind:lau-href=\"item.path\" v-bind:id=\"item.id\"><i class=\"layui-icon layui-icon-auz\"></i><cite>{{item.name}}</cite></a>\n                        <children-menu v-if=\"item.children!=null\" v-bind:data=\"item.children\"></children-menu>\n                    </li>\n             </ul>\n        </div>",
     props: {
         data: {
             type: Array,
@@ -8,7 +50,7 @@ Vue.component("left-menu", {
     }
 });
 Vue.component("children-menu", {
-    template: "\n            <ul class=\"sub-menu\">\n                <li v-for=\"(item,i) in data\">\n                    <a v-bind:_href=\"item.path\" v-bind:id=\"item.id\">\n                        <i class=\"iconfont\">&#xe6a7;</i>\n                        <cite>{{item.name}}</cite>\n                    </a>\n                    <children-menu v-if=\"item.children!=null\" v-bind:data=\"item.children\"></children-menu>\n                </li >\n            </ul>",
+    template: "\n            <dl class=\"lau-nav-child\">\n                <dd v-for=\"(item,i) in data\">\n                    <a v-bind:lau-href=\"item.path\" v-bind:id=\"item.id\" lau-event=\"reload\"><i class=\"layui-icon\"></i><cite>{{item.name}}</cite></a>\n                    <children-menu v-if=\"item.children!=null\" v-bind:data=\"item.children\"></children-menu>\n                </dd>\n            </dl>",
     props: {
         data: {
             type: Array,
--- src/main/resources/static/assets/chris/js/vue-component.ts
+++ src/main/resources/static/assets/chris/js/vue-component.ts
@@ -1,3 +1,4 @@
+/*
 
 Vue.component("left-menu",{
     template:`
@@ -31,6 +32,41 @@ Vue.component("children-menu",{
                     <children-menu v-if="item.children!=null" v-bind:data="item.children"></children-menu>
                 </li >
             </ul>`,
+    props:{
+        data: {
+            type: Array,
+            default: []
+        }
+    }
+});
+*/
+Vue.component("left-menu",{
+    template:`
+        <div class="layui-side-scroll">
+            <ul class="layui-nav layui-nav-tree" id="ul" style="background-color:#20222a">
+                    <!--这里可以写菜单结构,也可以放空,使用异步加载-->
+                    <span class="layui-nav-bar" style="height: 0px; top: 0px; opacity: 0;"></span>
+                    <li class="lau-nav-item" v-for="(item,i) in data">
+                        <a class="lau-nav-header" v-bind:lau-href="item.path" v-bind:id="item.id"><i class="layui-icon layui-icon-auz"></i><cite>{{item.name}}</cite></a>
+                        <children-menu v-if="item.children!=null" v-bind:data="item.children"></children-menu>
+                    </li>
+             </ul>
+        </div>`,
+    props:{
+        data: {
+            type: Array,
+            default: []
+        }
+    }
+});
+Vue.component("children-menu",{
+    template:`
+            <dl class="lau-nav-child">
+                <dd v-for="(item,i) in data">
+                    <a v-bind:lau-href="item.path" v-bind:id="item.id" lau-event="reload"><i class="layui-icon"></i><cite>{{item.name}}</cite></a>
+                    <children-menu v-if="item.children!=null" v-bind:data="item.children"></children-menu>
+                </dd>
+            </dl>`,
     props:{
         data: {
             type: Array,
