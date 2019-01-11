b89627310e4234eecb41f6ea4f09e114dbd91611
lihongshu <lhslxl@qq.com>
2019-01-11 17:40:35 +0800

    微信

---
+++ src/main/java/com/chris/bulleyeadmin/wechat/mapper/WxAccountMapper.java
@@ -0,0 +1,10 @@
+package com.chris.bulleyeadmin.wechat.mapper;
+
+
+import com.chris.bulleyeadmin.common.mapper.BaseMapper;
+import com.chris.bulleyeadmin.wechat.pojo.WxAccount;
+
+public interface WxAccountMapper extends BaseMapper<WxAccount> {
+
+
+}
