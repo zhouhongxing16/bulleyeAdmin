5fd7371cc48938e177cbe7dbce08c61ecdf193ba
lihongshu <lhslxl@qq.com>
2019-01-11 17:36:33 +0800

    微信调整

---
+++ src/main/java/com/chris/bulleyeadmin/wechat/pojo/WxAccount.java
@@ -0,0 +1,232 @@
+package com.chris.bulleyeadmin.wechat.pojo;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import javax.persistence.*;
+import java.util.Date;
+
+@Table(name = "wx_account")
+public class WxAccount {
+
+    private static final long serialVersionUID = 1L;
+    /**
+     * 唯一标识
+     */
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY, generator = "select uuid()")
+    private String id;
+
+    @Column(name = "account_pic")
+    private String accountPic;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "number")
+    private String number;
+
+    @Column(name = "appid")
+    private String appid;
+
+    @Column(name = "appsecret")
+    private String appsecret;
+
+    @Column(name = "token")
+    private String token;
+
+    @Column(name = "domain")
+    private String domain;
+
+    @Column(name = "access_token")
+    private String accessToken;
+
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss",timezone = "GMT+8")
+    private Date tokenTime;
+
+    @Column(name = "menu_state")
+    private String menuState;
+
+    @Column(name = "adduserid")
+    private Long adduserid;
+
+    @Column(name = "addusername")
+    private String addusername;
+
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss",timezone = "GMT+8")
+    private Date addtime;
+
+    @Column(name = "updateid")
+    private Long updateid;
+
+    @Column(name = "updatename")
+    private String updatename;
+
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss",timezone = "GMT+8")
+    private Date updatetime;
+
+    @Column(name = "remark")
+    private String remark;
+
+    @Column(name = "main_account")
+    private String mainAccount;
+
+    @Column(name = "wx_partner")
+    private String wxPartner;
+
+    @Column(name = "wx_partnerkey")
+    private String wxPartnerkey;
+
+    @Column(name = "wx_file")
+    private String wxFile;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getAccountPic() {
+        return accountPic;
+    }
+
+    public void setAccountPic(String accountPic) {
+        this.accountPic = accountPic;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public String getAppid() {
+        return appid;
+    }
+
+    public void setAppid(String appid) {
+        this.appid = appid;
+    }
+
+    public String getAppsecret() {
+        return appsecret;
+    }
+
+    public void setAppsecret(String appsecret) {
+        this.appsecret = appsecret;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public String getDomain() {
+        return domain;
+    }
+
+    public void setDomain(String domain) {
+        this.domain = domain;
+    }
+
+    public String getAccessToken() {
+        return accessToken;
+    }
+
+    public void setAccessToken(String accessToken) {
+        this.accessToken = accessToken;
+    }
+
+    public Date getTokenTime() {
+        return tokenTime;
+    }
+
+    public void setTokenTime(Date tokenTime) {
+        this.tokenTime = tokenTime;
+    }
+
+    public String getMenuState() {
+        return menuState;
+    }
+
+    public void setMenuState(String menuState) {
+        this.menuState = menuState;
+    }
+
+    public Long getAdduserid() {
+        return adduserid;
+    }
+
+    public void setAdduserid(Long adduserid) {
+        this.adduserid = adduserid;
+    }
+
+    public String getAddusername() {
+        return addusername;
+    }
+
+    public void setAddusername(String addusername) {
+        this.addusername = addusername;
+    }
+
+    public Date getAddtime() {
+        return addtime;
+    }
+
+    public void setAddtime(Date addtime) {
+        this.addtime = addtime;
+    }
+
+    public Long getUpdateid() {
+        return updateid;
+    }
+
+    public void setUpdateid(Long updateid) {
+        this.updateid = updateid;
+    }
+
+    public String getUpdatename() {
+        return updatename;
+    }
+
+    public void setUpdatename(String updatename) {
+        this.updatename = updatename;
+    }
+
+    public Date getUpdatetime() {
+        return updatetime;
+    }
+
+    public void setUpdatetime(Date updatetime) {
+        this.updatetime = updatetime;
+    }
+
+    public String getRemark() {
+        return remark;
+    }
+
+    public void setRemark(String remark) {
+        this.remark = remark;
+    }
+
+    public String getMainAccount() {
+        return mainAccount;
+    }
+
+    public void setMainAccount(String mainAccount) {
+        this.mainAccount = mainAccount;
+    }
+}
--- src/main/resources/db/migration/V1_3__wechat.sql
+++ src/main/resources/db/migration/V1_3__wechat.sql
@@ -1,11 +1,23 @@
+-- --------------------------------------------------------
+-- 主机:                           127.0.0.1
+-- 服务器版本:                        5.5.21 - MySQL Community Server (GPL)
+-- 服务器操作系统:                      Win64
+-- HeidiSQL 版本:                  9.1.0.4894
+-- --------------------------------------------------------
 
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+
+-- 导出 bulleye_admin 的数据库结构
 CREATE DATABASE IF NOT EXISTS `bulleye_admin` /*!40100 DEFAULT CHARACTER SET utf8 */;
 USE `bulleye_admin`;
 
 
 -- 导出  表 bulleye_admin.wx_account 结构
 CREATE TABLE IF NOT EXISTS `wx_account` (
-  `account_id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '公众号id',
+  `id` varchar(40) NOT NULL COMMENT '公众号id',
   `account_pic` varchar(500) DEFAULT NULL COMMENT '公众号二维码',
   `name` varchar(50) DEFAULT NULL COMMENT '名称',
   `number` varchar(50) DEFAULT NULL COMMENT '微信号',
@@ -16,23 +28,30 @@ CREATE TABLE IF NOT EXISTS `wx_account` (
   `access_token` varchar(50) DEFAULT NULL COMMENT 'access_token',
   `token_time` datetime DEFAULT NULL COMMENT '上次获取access_token的时间',
   `menu_state` varchar(50) DEFAULT NULL COMMENT 'menu_state',
-  `adduserid` bigint(20) DEFAULT NULL COMMENT '添加人ID',
+  `adduserid` varchar(50) DEFAULT NULL COMMENT '添加人ID',
   `addusername` varchar(50) DEFAULT NULL COMMENT '添加人NAME',
-  `addtime` varchar(50) DEFAULT NULL COMMENT '添加时间',
-  `updateid` bigint(20) DEFAULT NULL COMMENT '修改人ID',
+  `addtime` datetime DEFAULT NULL COMMENT '添加时间',
+  `updateid` varchar(50) DEFAULT NULL COMMENT '修改人ID',
   `updatename` varchar(50) DEFAULT NULL COMMENT '修改人NAME',
-  `updatetime` varchar(50) DEFAULT NULL COMMENT '修改时间',
+  `updatetime` timestamp NULL DEFAULT NULL COMMENT '修改时间',
   `remark` varchar(500) DEFAULT NULL COMMENT '备注',
   `main_account` varchar(50) DEFAULT NULL COMMENT '主公众号',
   `wx_partner` varchar(255) DEFAULT NULL COMMENT '微信商户id',
   `wx_partnerkey` varchar(255) DEFAULT NULL COMMENT '微信商户支付秘钥',
   `wx_file` varchar(255) DEFAULT NULL COMMENT '证书文件',
-  PRIMARY KEY (`account_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8 COMMENT='微信公众号信息';
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='微信公众号信息';
 
--- 导出  表 bulleye_admin.wx_member 结构
-CREATE TABLE IF NOT EXISTS `wx_member` (
-  `w_id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '微信用户id',
+-- 正在导出表  bulleye_admin.wx_account 的数据：~1 rows (大约)
+/*!40000 ALTER TABLE `wx_account` DISABLE KEYS */;
+INSERT INTO `wx_account` (`account_id`, `account_pic`, `name`, `number`, `appid`, `appsecret`, `token`, `domain`, `access_token`, `token_time`, `menu_state`, `adduserid`, `addusername`, `addtime`, `updateid`, `updatename`, `updatetime`, `remark`, `main_account`, `wx_partner`, `wx_partnerkey`, `wx_file`) VALUES
+	('3', '', 'lhs', 'gh_1065c22dc61c', 'wx0a8f9a49c445fd2b', '5aaea0afe5b65d60892a475bf43d668b', 'myToken', 'orcalone.imwork.net', NULL, NULL, '512f55b5-c5be-4b46-b9c7-b68218a3a1cf', '8', 'admin', '2018-10-25 11:39:51', '8', 'admin', '2018-10-25 11:40:09', '', '1', '', '', '');
+/*!40000 ALTER TABLE `wx_account` ENABLE KEYS */;
+
+
+-- 导出  表 bulleye_admin.wx_menber 结构
+CREATE TABLE IF NOT EXISTS `wx_menber` (
+  `id` varchar(40) NOT NULL COMMENT '微信用户id',
   `account_id` varchar(50) DEFAULT NULL COMMENT '所属公众号id',
   `account_name` varchar(50) DEFAULT NULL COMMENT '所属公众号名称',
   `subscribe` varchar(50) DEFAULT NULL COMMENT '关注状态 1.关注中 2.未关注',
@@ -43,18 +62,22 @@ CREATE TABLE IF NOT EXISTS `wx_member` (
   `country` varchar(50) DEFAULT NULL COMMENT '国家',
   `province` varchar(50) DEFAULT NULL COMMENT '省份',
   `headimgurl` varchar(500) DEFAULT NULL COMMENT '头像',
-  `createTime` varchar(50) DEFAULT NULL COMMENT '创建时间',
+  `createTime` datetime DEFAULT NULL COMMENT '创建时间',
   `user_id` varchar(50) DEFAULT NULL COMMENT '关联用户账户id',
-  `code` varchar(100) DEFAULT NULL COMMENT '永久二维码凑得',
-  PRIMARY KEY (`w_id`),
+  `code` varchar(100) DEFAULT NULL COMMENT '永久二维码code',
+  PRIMARY KEY (`id`),
   KEY `openid` (`openid`)
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8 COMMENT='微信用户';
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='微信用户';
+
+-- 正在导出表  bulleye_admin.wx_menber 的数据：~0 rows (大约)
+/*!40000 ALTER TABLE `wx_menber` DISABLE KEYS */;
+/*!40000 ALTER TABLE `wx_menber` ENABLE KEYS */;
 
 
 -- 导出  表 bulleye_admin.wx_reply 结构
 CREATE TABLE IF NOT EXISTS `wx_reply` (
-  `reply_id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT 'ID',
-  `account_id` bigint(20) DEFAULT NULL COMMENT '所属公众号ID',
+  `id` varchar(40) NOT NULL COMMENT 'ID',
+  `account_id` varchar(40) DEFAULT NULL COMMENT '所属公众号ID',
   `reply_keyword` varchar(100) DEFAULT NULL COMMENT '关键字',
   `reply_title` varchar(100) DEFAULT NULL COMMENT '标题',
   `reply_pic` varchar(150) DEFAULT NULL COMMENT '图片',
@@ -62,6 +85,12 @@ CREATE TABLE IF NOT EXISTS `wx_reply` (
   `reply_content` varchar(250) DEFAULT NULL COMMENT '内容',
   `reply_type` varchar(50) DEFAULT NULL COMMENT '类型 1.图文 2.纯文字',
   `reply_num` bigint(20) DEFAULT NULL COMMENT '回复次数',
-  PRIMARY KEY (`reply_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COMMENT='微信自动回复';
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='微信自动回复';
 
+-- 正在导出表  bulleye_admin.wx_reply 的数据：~0 rows (大约)
+/*!40000 ALTER TABLE `wx_reply` DISABLE KEYS */;
+/*!40000 ALTER TABLE `wx_reply` ENABLE KEYS */;
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
