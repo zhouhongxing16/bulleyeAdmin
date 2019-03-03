57bc1d1d2edf89ad379eff5172bceff86536d4d5
961860916@qq.com <8302812>
2019-03-03 14:24:03 +0800

    增加微信相关接口

--- src/main/java/com/chris/bulleyeadmin/wechat/controller/WxMenuController.java
+++ src/main/java/com/chris/bulleyeadmin/wechat/controller/WxMenuController.java
@@ -1,4 +1,27 @@
 package com.chris.bulleyeadmin.wechat.controller;
 
-public class WxMenuController {
+import com.chris.bulleyeadmin.common.controller.BaseController;
+import com.chris.bulleyeadmin.common.service.BaseService;
+import com.chris.bulleyeadmin.wechat.pojo.WxMenu;
+import com.chris.bulleyeadmin.wechat.service.WxMenuService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/wxmenu")
+public class WxMenuController extends BaseController<WxMenu> {
+
+    @Autowired
+    WxMenuService wxMenuService;
+
+    @Override
+    public BaseService<WxMenu> getService() {
+        return wxMenuService;
+    }
+
+    @Override
+    public String getViewPrefix() {
+        return "wxmenu";
+    }
 }
