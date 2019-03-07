3c01f216795cd508241ccc7545145dbea8bd15a9
lihongshu <lhslxl@qq.com>
2019-03-07 22:47:15 +0800

    删除不需要的方法

--- src/main/java/com/chris/bulleyeadmin/wechat/service/WxMenuService.java
+++ src/main/java/com/chris/bulleyeadmin/wechat/service/WxMenuService.java
@@ -38,10 +38,6 @@ public class WxMenuService extends BaseService<WxMenu> {
         return wxMenuMapper.selectlist(wxMenu);
     }
 
-    public List<WxMenu> selectTwolist(WxMenu wxMenu) {
-        return wxMenuMapper.selectTwolist(wxMenu);
-    }
-
     public String createWxMenu(String appId){
         WxMpService wxService = WxMpConfiguration.getMpServices().get(appId);
 
