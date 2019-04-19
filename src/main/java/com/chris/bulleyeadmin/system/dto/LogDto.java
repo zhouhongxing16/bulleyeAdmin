ba817062b9ee886e1cd7534f5bd30d3e18223109
961860916@qq.com <8302812>
2019-04-19 14:26:04 +0800

    增加日志DTO

---
+++ src/main/java/com/chris/bulleyeadmin/system/dto/LogDto.java
@@ -0,0 +1,10 @@
+package com.chris.bulleyeadmin.system.dto;
+import com.chris.bulleyeadmin.system.pojo.Logs;
+
+/**
+ * @Author: Chris  E-mail:961860916@qq.com
+ * @Date:  2019-04-19 13:25
+ */
+public class LogDto extends Logs {
+
+}
