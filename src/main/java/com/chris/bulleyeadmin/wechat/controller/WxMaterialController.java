4e69b2ad31fdc89085a9156ef0f1508b22533620
lhslxl <5473438+lhslxl@user.noreply.gitee.com>
2020-03-31 17:29:19 +0800

    微信素材功能调整

--- src/main/java/com/chris/bulleyeadmin/wechat/controller/WxMaterialController.java
+++ src/main/java/com/chris/bulleyeadmin/wechat/controller/WxMaterialController.java
@@ -10,7 +10,6 @@ import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
 
 @OperationLog("素材管理")
 @RestController
@@ -25,33 +24,25 @@ public class WxMaterialController extends BaseController<WxMaterial> {
         return wxMaterialService;
     }
 
-    @ApiOperation(value = "创建方法", notes = "创建")
-    @ApiImplicitParam(name = "创建方法", value = "参数如果有时间字段请按照 yyyy-MM-dd hh:mm:ss 格式传入")
-    @OperationLog("创建方法")
-    @PostMapping("/addMaterial")
-    @ResponseBody
-    public JsonResult addMaterial(@RequestBody WxMaterial obj) throws Exception {
-        //新增时记录本地文件id
-        String file_id= obj.getThumbMediaId();
-        obj.setThumbFileId(file_id);
-        obj.setThumbMediaId("");
-        return super.create(obj);
-    }
-
-    @ApiOperation(value = "创建方法", notes = "创建")
-    @ApiImplicitParam(name = "创建方法", value = "参数如果有时间字段请按照 yyyy-MM-dd hh:mm:ss 格式传入")
-    @OperationLog("创建方法")
+    @ApiOperation(value = "修改方法", notes = "创建")
+    @ApiImplicitParam(name = "修改方法", value = "参数如果有时间字段请按照 yyyy-MM-dd hh:mm:ss 格式传入")
+    @OperationLog("修改方法")
     @PostMapping("/updateMaterial")
     @ResponseBody
     public JsonResult updateMaterial(@RequestBody WxMaterial obj) throws Exception {
-        //修改时记录本地文件id
-        String file_id= obj.getThumbMediaId();
-        obj.setThumbFileId(file_id);
-        obj.setThumbMediaId("");
         return wxMaterialService.updateMaterial(obj);
     }
 
-    @OperationLog("生成永久素材")
+    //删除
+    @ApiOperation(value = "根据ID删除一条数据", notes = "根据ID删除一条数据")
+    @GetMapping("/delete/{id}")
+    @ResponseBody
+    @OperationLog("删除")
+    public JsonResult remove(@PathVariable String id) {
+        return getService().deleteById(id);
+    }
+
+    //@OperationLog("生成永久素材")
     @GetMapping("/materialUpload/{id}")
     public JsonResult materialUpload(@PathVariable String id) {
         return wxMaterialService.materialUpload(id);
