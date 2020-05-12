599d6db14181422e746bbf17602abe312d9af73d
961860916@qq.com <8302812>
2020-05-12 18:11:52 +0800

    优化

--- src/main/java/com/chris/bulleyeadmin/system/dto/StaffDto.java
+++ src/main/java/com/chris/bulleyeadmin/system/dto/StaffDto.java
@@ -8,4 +8,24 @@ import com.chris.bulleyeadmin.system.pojo.Staff;
  * @Description:
  */
 public class StaffDto extends Staff {
+
+    private String genderName;
+
+    private String titleName;
+
+    public String getGenderName() {
+        return genderName;
+    }
+
+    public void setGenderName(String genderName) {
+        this.genderName = genderName;
+    }
+
+    public String getTitleName() {
+        return titleName;
+    }
+
+    public void setTitleName(String titleName) {
+        this.titleName = titleName;
+    }
 }
