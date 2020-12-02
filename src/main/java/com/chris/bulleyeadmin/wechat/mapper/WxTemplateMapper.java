7ec48aedb61b109533393df80df866315cb7d2f0
lhslxl <5473438+lhslxl@user.noreply.gitee.com>
2020-12-02 10:33:25 +0800

    tempalte

---
+++ src/main/java/com/chris/bulleyeadmin/wechat/mapper/WxTemplateMapper.java
@@ -0,0 +1,7 @@
+package com.chris.bulleyeadmin.wechat.mapper;
+
+import com.chris.bulleyeadmin.common.basemapper.BaseMapper;
+import com.chris.bulleyeadmin.wechat.pojo.WxTemplate;
+
+public interface WxTemplateMapper extends BaseMapper<WxTemplate> {
+}
