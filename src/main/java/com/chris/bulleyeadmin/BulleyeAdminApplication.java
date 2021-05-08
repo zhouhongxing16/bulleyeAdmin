5eaf935230bed76fa2ee1a819dae83bfc7204bd9
Chris <961860916@qq.com>
2021-05-08 09:43:00 +0800

    打印激活配置文件

--- src/main/java/com/chris/bulleyeadmin/BulleyeAdminApplication.java
+++ src/main/java/com/chris/bulleyeadmin/BulleyeAdminApplication.java
@@ -1,14 +1,22 @@
 package com.chris.bulleyeadmin;
 
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 import tk.mybatis.spring.annotation.MapperScan;
 
 @SpringBootApplication
 @MapperScan(basePackages = "com.chris.bulleyeadmin.**.mapper")
 public class BulleyeAdminApplication {
 
+
     public static void main(String[] args) {
-        SpringApplication.run(BulleyeAdminApplication.class, args);
+        final Logger logger = LoggerFactory.getLogger(BulleyeAdminApplication.class);
+        ApplicationContext ctx = SpringApplication.run(BulleyeAdminApplication.class, args);
+        String[] activeProfiles = ctx.getEnvironment().getActiveProfiles();
+        logger.info("当前使用的 profile 是:{}", StringUtils.join("、",activeProfiles));
     }
 }
