39d23b860e6c9baaa87de19409916776ea80c319
Chris <961860916@qq.com>
2021-05-12 14:17:08 +0800

    去掉账号信息

--- src/main/java/com/chris/bulleyeadmin/common/utils/SnowFlake.java
+++ src/main/java/com/chris/bulleyeadmin/common/utils/SnowFlake.java
@@ -78,7 +78,7 @@ public class SnowFlake {
         return (currStmp - START_STMP) << TIMESTMP_LEFT //时间戳部分
                 | datacenterId << DATACENTER_LEFT       //数据中心部分
                 | machineId << MACHINE_LEFT             //机器标识部分
-                | sequence;                             //序列号部分
+                | sequence ;                             //序列号部分
     }
 
     private long getNextMill() {
---
+++ src/test/java/com/chris/bulleyeadmin/SpringBootRedisApplicationTests.java
@@ -0,0 +1,26 @@
+package com.chris.bulleyeadmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
+
+/**
+ * @author Chris
+ * @date 2021-05-12 13:48
+ */
+@SpringBootTest
+class SpringBootRedisApplicationTests {
+
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
+
+    @Test
+    void contextLoads() {
+        ValueOperations<String, String> opsForValue = this.stringRedisTemplate.opsForValue();
+        opsForValue.set("name", "lisi"); // 缓存数据
+        String value = opsForValue.get("name"); // 获取缓存数据
+        System.out.println(value);
+    }
+}
